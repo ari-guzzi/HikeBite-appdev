e8308bd9916678e24ab7b64c75c612c8511c2066
Ari Guzzi <argu4451@colorado.edu>
2025-03-17 15:06:54 -0600

    added folder for Will

---
+++ HikeBite/HikeBite/HowToSlides/WelcomeToHikeBite.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeToHikeBite.swift
+//  HikeBite
+//
+//  Created by Ari Guzzi on 3/17/25.
+//
+
+import SwiftUI
+//this your file Will :)
+struct WelcomeToHikeBite: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeToHikeBite()
+}
