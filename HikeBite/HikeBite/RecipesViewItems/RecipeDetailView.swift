ffec09dddeb797adb384f29c189054e5e03af9e1
Ari Guzzi <argu4451@colorado.edu>
2025-03-26 17:13:59 -0600

    did recipedetail aethetic

--- HikeBite/HikeBite/RecipesViewItems/RecipeDetailView.swift
+++ HikeBite/HikeBite/RecipesViewItems/RecipeDetailView.swift
@@ -4,13 +4,13 @@
 //
 //  Created by Ari Guzzi on 1/13/25.
 //
-import Foundation
-import SwiftData
-import SwiftUI
 import Combine
 import Firebase
 import FirebaseFirestore
 import FirebaseStorage
+import Foundation
+import SwiftData
+import SwiftUI
 
 struct RecipeDetailView: View {
     var recipe: Result
@@ -26,143 +26,146 @@ struct RecipeDetailView: View {
     let meals = ["Breakfast", "Lunch", "Dinner", "Snacks"]
     @State private var image: Image?
     @State private var imageURL: URL?
-
     var body: some View {
-        VStack {
+        NavigationView {
             ScrollView {
                 VStack(alignment: .leading) {
-                    Text(recipe.title)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding(.top)
-                    AsyncImage(url: imageURL) { phase in
-                        switch phase {
-                        case .success(let image):
-                            image.resizable()
-                                 .aspectRatio(contentMode: .fill)
-                                 .frame(width: 100, height: 100)
-                                 .cornerRadius(8)
-                        case .empty:
-                            ProgressView()
-                        case .failure(_):
-                            Text("Unable to load image")
-                            .frame(width: 100, height: 100)
-                            .background(Color.gray)
-                            .cornerRadius(8)
-                        @unknown default:
-                            EmptyView()
-                        }
-                    }
-//                    if let imageURL = imageURL {
-//                        AsyncImage(url: imageURL) { phase in
-//                            switch phase {
-//                            case .success(let image):
-//                                image.resizable()
-//                                     .aspectRatio(contentMode: .fill)
-//                                     .frame(width: 300, height: 200)
-//                                     .cornerRadius(10)
-//                            case .failure(_):
-//                                Text("Unable to load image")
-//                                    .foregroundColor(.red)
-//                                    .frame(width: 300, height: 200)
-//                            case .empty:
-//                                ProgressView()
-//                                    .frame(width: 300, height: 200)
-//                            @unknown default:
-//                                EmptyView()
-//                            }
-//                        }
-//                    } else {
-//                        Color.gray.opacity(0.3)
-//                            .frame(width: 300, height: 200)
-//                            .cornerRadius(10)
-//                    }
-
-                        if !recipe.description.isEmpty {
-                            Text(recipe.description)
-                                .font(.body)
-                                .foregroundColor(.gray)
+                    ZStack {
+                        LinearGradient(gradient: Gradient(colors: [Color(red: 0.67, green: 0.85, blue: 0.76),.white]),
+                                       startPoint: .bottom,
+                                       endPoint: .top)
+                        .frame(height: UIScreen.main.bounds.height * 0.6)
+                        .edgesIgnoringSafeArea([.all])
+                        FunnyLines()
+                            .ignoresSafeArea(.all)
+                        VStack {
+                            Text(recipe.title)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .padding(.top)
+                            if let imageURL = imageURL {
+                                AsyncImage(url: imageURL) { phase in
+                                    switch phase {
+                                    case .success(let image):
+                                        image.resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: 300, height: 200)
+                                            .cornerRadius(10)
+                                    case .failure(_):
+                                        Text("Unable to load image")
+                                            .frame(width: 300, height: 200)
+                                            .background(Color.gray)
+                                    case .empty:
+                                        ProgressView()
+                                            .frame(width: 300, height: 200)
+                                    @unknown default:
+                                        EmptyView()
+                                    }
+                                }
+                            } else {
+                                Color.gray.opacity(0.3)
+                                    .frame(width: 300, height: 200)
+                                    .cornerRadius(10)
+                            }
+                            if !recipe.description.isEmpty {
+                                Text(recipe.description)
+                                    .font(.body)
+                                    .foregroundColor(.gray)
+                                    .padding(.horizontal)
+                                    .padding(.top, 4)
+                            }
+                            if let createdBy = recipe.createdBy, !createdBy.isEmpty {
+                                HStack {
+                                    Text("Created by: ")
+                                        .font(.subheadline)
+                                        .foregroundColor(.secondary)
+                                    Text(createdBy)
+                                        .font(.subheadline)
+                                        .bold()
+                                }
                                 .padding(.horizontal)
-                                .padding(.top, 4)
-                        }
-                        if let createdBy = recipe.createdBy, !createdBy.isEmpty {
+                            }
+                            if let timestamp = recipe.timestamp {
+                                HStack {
+                                    Text("Uploaded on: ")
+                                        .font(.subheadline)
+                                        .foregroundColor(.secondary)
+                                    Text(timestamp.formatted(date: .abbreviated, time: .shortened))
+                                        .font(.subheadline)
+                                        .bold()
+                                }
+                                .padding(.horizontal)
+                            }
+                            if !recipe.filter.isEmpty {
+                                HStack {
+                                    Text("Filters:")
+                                        .font(.system(size: 24, weight: .bold))
+                                        .font(.title2)
+                                        .padding(.vertical, 4)
+                                    Spacer()
+                                }
+                                HStack {
+                                    ForEach(recipe.filter, id: \.self) { filter in
+                                        Text(filter)
+                                            .padding(8)
+                                            .background(Color(red: 0.91, green: 1, blue: 0.96))
+                                            .cornerRadius(9)
+                                            .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                                    }
+                                }
+                                .padding(.bottom, 8)
+                            }
+                            
                             HStack {
-                                Text("Created by: ")
-                                    .font(.subheadline)
-                                    .foregroundColor(.secondary)
-                                Text(createdBy)
-                                    .font(.subheadline)
-                                    .bold()
+                                Text("Servings: \(servings)")
+                                    .font(.title2)
+                                    .font(.system(size: 24, weight: .bold))
+                                Spacer()
+                                Button(action: { if servings > 1 { servings -= 1 } }) {
+                                    Image(systemName: "minus.circle").foregroundColor(.red)
+                                }
+                                Button(action: { servings += 1 }) {
+                                    Image(systemName: "plus.circle").foregroundColor(.green)
+                                }
                             }
-                            .padding(.horizontal)
+                            .frame(width: UIScreen.main.bounds.width - 20)
                         }
-
-                        if let timestamp = recipe.timestamp {
-                            HStack {
-                                Text("Uploaded on: ")
-                                    .font(.subheadline)
-                                    .foregroundColor(.secondary)
-                                Text(timestamp.formatted(date: .abbreviated, time: .shortened)) // Formats date nicely
-                                    .font(.subheadline)
-                                    .bold()
-                            }
-                            .padding(.horizontal)
-                        }
-                    if !recipe.filter.isEmpty {
-                        Text("Filters:")
-                            .fontWeight(.bold)
-                            .font(.title2)
-                            .padding(.vertical, 4)
-                        HStack {
-                            ForEach(recipe.filter, id: \.self) { filter in
-                                Text(filter)
-                                    .padding(8)
-                                    .background(Color.gray.opacity(0.2))
-                                    .cornerRadius(8)
-                            }
-                        }
-                        .padding(.bottom, 8)
+                        .frame(width: UIScreen.main.bounds.width - 10)
                     }
-
-                    HStack {
-                        Text("Servings: \(servings)").font(.headline)
-                        Spacer()
-                        Button(action: { if servings > 1 { servings -= 1 } }) {
-                            Image(systemName: "minus.circle").foregroundColor(.red)
+                    ZStack {
+                        LinearGradient(gradient: Gradient(colors: [Color(red: 0.67, green: 0.85, blue: 0.76),.white]),
+                                       startPoint: .top,
+                                       endPoint: .center)
+                        .offset(y: -30)
+                        .edgesIgnoringSafeArea([.all])
+                        VStack {
+                            Text("Ingredients:")
+                                .font(.title2)
+                                .font(.system(size: 24, weight: .bold))
+                                .font(.title2)
+                                .padding(.vertical)
+                            ForEach(mutableIngredients.indices, id: \.self) { index in
+                                viewIngredient(ingredient: $mutableIngredients[index], servings: servings)
+                            }
+                            .frame(width: UIScreen.main.bounds.width - 50)
+                            Button(action: {
+                                showAddToPlanSheet = true
+                            }) {
+                                Text("Add to My Plan")
+                                    .font(.headline)
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                                    .padding()
+                            }
                         }
-                        Button(action: { servings += 1 }) {
-                            Image(systemName: "plus.circle").foregroundColor(.green)
-                        }
-                    }
-                    .padding()
-
-                    Text("Ingredients:")
-                        .fontWeight(.bold)
-                        .font(.title2)
-                        .padding(.vertical)
-
-                    ForEach(mutableIngredients.indices, id: \.self) { index in
-                        viewIngredient(ingredient: $mutableIngredients[index], servings: servings)
                     }
                 }
-                .navigationTitle("Recipe Details")
-                .navigationBarTitleDisplayMode(.inline)
-                .padding()
             }
-
-            Button(action: {
-                showAddToPlanSheet = true
-            }) {
-                Text("Add to My Plan")
-                    .font(.headline)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .padding()
-            }
+            .navigationBarTitle("Meal Details", displayMode: .inline)
         }
         .sheet(isPresented: $showAddToPlanSheet) {
             MealSelectionView(selectedDay: $selectedDay, selectedMeal: $selectedMeal, servings: $servings) {
@@ -171,41 +174,57 @@ struct RecipeDetailView: View {
         }
         .onAppear {
             mutableIngredients = recipe.ingredients
-            loadRecipeImage()
+            if let urlString = recipe.img, let imageUrl = URL(string: urlString) {
+                self.imageURL = imageUrl
+                print("Using URL: \(imageUrl)")
+            } else {
+                print("Invalid URL string: \(recipe.img ?? "nil")")
             }
         }
+    }
     func loadRecipeImage() {
         guard let imgPath = recipe.img else {
             print("No image path available.")
             return
         }
         getDownloadURL(for: imgPath) { url in
-            if let urlString = url, let url = URL(string: urlString) {
-                DispatchQueue.main.async {
-                    self.imageURL = url
-                    print(url)
+            if let url = url {
+                self.imageURL = url
+                print("URL set for AsyncImage: \(url)")
+            } else {
+                print("Failed to load URL for image path: \(imgPath)")
+            }
+        }
+    }
+    func getDownloadURL(for imageUrlString: String, completion: @escaping (URL?) -> Void) {
+        guard let url = URL(string: imageUrlString) else {
+            print("Invalid URL: \(imageUrlString)")
+            completion(nil)
+            return
+        }
+        let pathComponents = url.pathComponents
+        // This might need adjustment depending on your specific URL structure
+        let imagePath = pathComponents.suffix(2).joined(separator: "/")
+        
+        let storageRef = Storage.storage().reference(withPath: imagePath)
+        storageRef.downloadURL { url, error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    print("Error fetching URL: \(error.localizedDescription)")
+                    completion(nil)
+                } else if let url = url {
+                    print("Fetched URL: \(url)")
+                    completion(url)
                 }
             }
         }
     }
-
-    func getDownloadURL(for imageName: String, completion: @escaping (String?) -> Void) {
-        let storageRef = Storage.storage().reference(withPath: imageName)
-        storageRef.downloadURL { url, error in
-            if let error = error {
-                print("Error fetching URL: \(error.localizedDescription)")
-                completion(nil)
-            } else if let url = url {
-                completion(url.absoluteString)  // This is the HTTP URL
-            }
-        }
-    }
+    
     private func addRecipeToPlan() {
         guard let selectedTrip = selectedTrip else {
             print("❌ No trip selected!")
             return
         }
-
         let totalCalories = mutableIngredients.reduce(0) { $0 + (($1.calories ?? 0) * servings) }
         let totalGrams = mutableIngredients.reduce(0) { $0 + (($1.weight ?? 0) * servings) }
         let newMealEntry = MealEntry(
@@ -229,7 +248,7 @@ struct RecipeDetailView: View {
         let adjustedAmount = (ingredient.wrappedValue.amount ?? 0.0) * Double(servings)
         let formattedAmount = "\(String(format: "%.1f", adjustedAmount)) \(ingredient.wrappedValue.unit)"
         let groceryItemName = "\(formattedAmount) of \(ingredientName)"
-
+        
         HStack {
             VStack(alignment: .leading) {
                 Text(ingredientName).fontWeight(.bold)
@@ -238,11 +257,11 @@ struct RecipeDetailView: View {
                 Text("Weight: \((ingredient.wrappedValue.weight ?? 0) * servings) g").font(.caption)
             }
             Spacer()
-
+            
             Button {
                 let newGroceryItem = GroceryItem(name: groceryItemName, isCompleted: false)
                 modelContext.insert(newGroceryItem)
-
+                
                 do {
                     try modelContext.save()
                     print("✅ Added to grocery list: \(newGroceryItem.name)")
@@ -256,27 +275,10 @@ struct RecipeDetailView: View {
             }
         }
         .padding()
-        .border(Color(hue: 1.0, saturation: 0.023, brightness: 0.907))
+            .cornerRadius(9) 
+            .overlay(
+                RoundedRectangle(cornerRadius: 9)
+                    .stroke(Color(red: 0, green: 0.41, blue: 0.22), lineWidth: 1)
+                )
     }
-
-    }
-
-struct RecipeSearchResponse: Codable {
-    let results: [Recipe]
-    let offset: Int
-    let number: Int
-    let totalResults: Int
-}
-
-struct Recipe: Codable, Identifiable {
-    let id: Int
-    let title: String
-    let image: String
-    let imageType: String
-    let readyInMinutes: Int?
-    let servings: Int?
-    let sourceUrl: String?
-    // Include other fields as necessary, ensuring they are marked optional if they can be absent
-
-    var identifier: String { return "\(id)" }
 }
